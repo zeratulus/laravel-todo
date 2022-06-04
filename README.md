0e8ba4d16e98620fee57d63c198ffd7fbf5ffddd
Serhii Herenko <ailus665@gmail.com>
2022-06-04 22:09:42 +0300

    Added laravel/ui, Vue.js, Task model, controller and routes.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+
+How to run
+  - Make .env file with your DB configurations.
+  - Run in console: composer install
+  - php artisan migrate
+  - Run in console: npm run build
+  - 
+
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
 
 <p align="center">
---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all()->toArray();
+        return array_reverse($tasks);
+    }
+
+    public function store(Request $request)
+    {
+        $task = new Task();
+        $task->fill($request->input());
+        $task->save();
+        return response()->json('Task created!');
+    }
+
+    public function show($id)
+    {
+        $task = Task::find($id);
+        return response()->json($task);
+    }
+
+    public function update($id, Request $request)
+    {
+        $task = Task::find($id);
+        $task->update($request->all());
+        return response()->json('Task updated!');
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+        return response()->json('Task deleted!');
+    }
+}
